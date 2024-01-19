d8f0d8c5eef20be1f0fc4f7a3f95bed4c97de1cf
Kierthana <kierthanars@gmail.com>
2024-01-19 16:47:46 +0530

    Pushing to BTS

---
+++ server/server.js
@@ -0,0 +1,117 @@
+const express = require('express');
+const mysql = require('mysql');
+
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+
+app.use(cors());
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'bts' 
+}); 
+
+
+db.connect((err) => {
+    if(err){
+        throw err;
+    }
+    console.log('MySql Connected...');
+}
+);
+
+app.get('/songs', (req, res) => {
+    const query= "SELECT id,name FROM songs"; 
+    db.query(query, (err, result) => {
+        if(err){
+            console.error('Error executing query: ' + err.stack);
+            res.status(500).send('Internal Server Error');
+        } 
+        else{ 
+           
+            res.send({songs:result});
+        }
+    });
+});
+
+app.get('/getSongById/:id', (req, res) => {
+    const query= "SELECT name,Lyrics,musicUrl,pic FROM songs WHERE id = ?";
+    db.query(query, [req.params.id], (err, result) => {
+        if(err){
+            console.error('Error executing query: ' + err.stack);
+            res.status(500).send('Internal Server Error');
+        } 
+        else{
+            res.send(result);
+        }
+    });
+});
+
+app.get('/visitercount', (req, res) => {  
+    const updatequery= "UPDATE visitercount SET count = count + 1 WHERE id = 1"; 
+    const query= "SELECT count FROM visitercount WHERE id = 1";
+    db.query(updatequery, (err, result) => {
+        if(err){
+            console.error('Error executing query: ' + err.stack);
+            res.status(500).send('Internal Server Error');
+        } 
+        else{
+            db.query(query, (err, result) => {
+                if(err){
+                    console.error('Error executing query: ' + err.stack);
+                    res.status(500).send('Internal Server Error');
+                } 
+                else{
+                    res.send(result);
+                    
+                }
+            });
+        }
+    });
+
+});
+app.post('/messagesadd',(req,res)=>{   
+    const query= "INSERT INTO chats (username,chat,date) VALUES (?,?,DATE(NOW()))";
+    db.query(query, [req.body.user,req.body.text], (err, result) => {
+        if(err){
+            console.error('Error executing query: ' + err.stack);
+            res.status(500).send('Internal Server Error');
+        } 
+        else{
+            res.send(result);
+        }
+    });
+}); 
+ 
+app.get('/messages', (req, res) => {
+    const query= "SELECT username,chat,date FROM chats"; 
+    db.query(query, (err, result) => {
+        if(err){
+            console.error('Error executing query: ' + err.stack);
+            res.status(500).send('Internal Server Error');
+        } 
+        else{ 
+           
+            res.send({messages:result});
+        }
+    });
+});
+
+app.get('/album/:id', (req, res) => {
+    const query= "SELECT * FROM album WHERE id = ?";
+    db.query(query, [req.params.id], (err, result) => {
+        if(err){
+            console.error('Error executing query: ' + err.stack);
+            res.status(500).send('Internal Server Error');
+        } 
+        else{
+            res.send(result);
+        }
+    });
+});
+app.listen('5000', () => {
+    console.log('Server started on port 5000');
+});
